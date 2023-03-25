5b52ce1d0a99ae78aaf11de9ce0f15539a8a6cad
mostafa21314 <mostafa21314@aucegypt.edu>
2023-03-26 00:20:09 +0200

    finalizing everything

--- CS2_ASSGN_3/main.cpp
+++ CS2_ASSGN_3/main.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 int main()
 {
-	const int Mnum = 3;
-	const int Cnum = 5;
+	const int Mnum = 3;//number of mechanics in the workshop
+	const int Cnum = 5;//number of customers in the workshop
 	Mechanic workers[Mnum];
 	workers[0].Mechanic::Mechanic("Mostafa", "15", 19);
 	workers[1].Mechanic::Mechanic("Ahmed", "16", 20);
 	workers[2].Mechanic::Mechanic("Mahmoud", "17", 18);
-	/*for (int i = 0; i < Mnum; i++)
+	/*for (int i = 0; i < Mnum; i++)//loop for the user to enter the data of the mechanic, a similar loop can be implemented for customers
 	{
 		int num;
 		string name, ID;
@@ -21,31 +21,31 @@ int main()
 		cin >> name >> num >> ID;
 		workers[i].Mechanic::Mechanic(name,ID,num);
 	}*/
-	Customer money[Cnum];
-	for (int i = 0; i < Cnum; i++)
+	Customer money[Cnum];//array of customers declared
+	for (int i = 0; i < Cnum; i++)//loop to let the user enter the information of the customer, the loop assigns the appropriate mechanic to the customer
 	{
 		int num,h,m;
 		string name, ID;
 		cout << "Please enter the customer information such as name age id appointment time(hours min):  ";
 		cin >> name >> num >> ID>>h>>m;
-		Appointment temp(h, m);
+		Appointment temp(h, m);//the appointments the customer needs
 		money[i].Customer::Customer(name, ID, num,"",temp);
 		int x=0;
-		for (x = 0; x < Mnum; x++)
+		for (x = 0; x < Mnum; x++)//makes sure that theres at least 1 mechanic available at the appointment time the customer chose
 		{
 			if (workers[x].IsAvailable(temp))
 				break;
 		}
-		if (x == Mnum)
+		if (x == Mnum)//if no mechanic was available at that time the appointment is changed to -1:-1 which triggers an output of no mechanic was availabe when print infor is used
 		{
 			Appointment non(-1, -1);
 			money[i].SetApp(non);
 		}
-		else
+		else//else means theres at least one mechanic available therefore the customer will be assigned to the appropriate mechanic
 		{
-			for (int y = i % Mnum; y > -3; y++)
+			for (int y = i % Mnum; y > -3; y++)//the % makes sure the customers loop back to the start of the mechanics if customers are more than the mechanics
 			{
-				if (workers[y].IsAvailable(temp))
+				if (workers[y].IsAvailable(temp))//assigns the appropriate mechanic to the customer
 				{
 					money[i].SetMID(workers[y].GetID());
 					workers[y].SetApp(temp);
@@ -54,8 +54,8 @@ int main()
 			}
 		}
 	}
-	Queue<Customer> line(Cnum);
-	for (int x = 0; x < Cnum; x++)
+	Queue<Customer> line(Cnum);//queue of customers
+	for (int x = 0; x < Cnum; x++)//loop to bubble sort the queue so that the customer with the earlier appointment is in front
 	{
 			for (int j = x + 1; j < Cnum; j++)
 			{
@@ -69,10 +69,19 @@ int main()
 			line.push(money[x]);
 	}
 
-	for (int i = 0; i < Cnum; i++)
+	for (int i = 0; i < Cnum; i++)//loop to cout the queue status
 	{
 		Customer test = line.pop();
-		cout << "Mr " << test.GetName() << " Has an Appointment at " << test.GetApp().hours << ":" << test.GetApp().min << " with mechanic whos ID is " << test.GetMID() << endl;
+		string Mechname="";
+		for (int i = 0; i < Mnum; i++)
+		{
+			if (test.GetMID() == workers[i].GetID())
+				Mechname = workers[i].GetName();
+		}
+		if (Mechname == "")
+			cout << "Mr " << test.GetName() << " Appointment was cancelled because no mechanic was available " << endl;
+		else
+		cout << "Mr " << test.GetName() << " Has an Appointment at " << test.GetApp().hours << ":" << test.GetApp().min << " with Mr " <<Mechname << endl;
 	}
 	return 0;
 }
